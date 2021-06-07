07138e4315ecdd1092d050502257262a05a6b5de
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-07 16:40:44 -0700

    getName test red

--- red-green-refactor.test.js
+++ red-green-refactor.test.js
@@ -1,1 +1,8 @@
 
+describe('getName', () => {
+  it('returns the name property of an object', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    const name = getName(spot);
+    expect(name).toEqual('spot');
+  });
+});

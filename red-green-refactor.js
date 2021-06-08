95ab36a3c05917bc70ae52189e11fdc0c364ff25
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-07 17:48:04 -0700

    redo capitalizeAndFilter w/ for loop

--- red-green-refactor.js
+++ red-green-refactor.js
@@ -7,7 +7,12 @@ export function copyAndPush(array, number) {
 }
 
 export function capitalizeAndFilter(array) {
-  return array.filter((item) => item[0] !== 'f'.toLowerCase()).map((item) => {
-    return item.toUpperCase();
-  });
+//   return array.filter((item) => item[0] !== 'f'.toLowerCase()).map((item) => {
+//     return item.toUpperCase();
+//   });
+  const newArray = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i][0] !== 'f') newArray.push(array[i].toUpperCase());
+  }     
+  return newArray;
 }

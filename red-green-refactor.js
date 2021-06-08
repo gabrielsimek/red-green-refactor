bc21d7a3fcd20ad3373b14b1c8779633fd070bff
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-07 17:34:38 -0700

    capitalizeAndFilter pass w/ array methods

--- red-green-refactor.js
+++ red-green-refactor.js
@@ -7,10 +7,7 @@ export function copyAndPush(array, number) {
 }
 
 export function capitalizeAndFilter(array) {
-  const newArray = [];
-  for(let i = 0; i < array.length; i++) {
-    if(array[i][0] === 'f'.toLowerCase()) array.splice(i, 0);
-    else newArray.push(array[i].toUpperCase());
-  }
-  return newArray;
+  return array.filter((item) => item[0] !== 'f'.toLowerCase()).map((item) => {
+    return item.toUpperCase();
+  });
 }
